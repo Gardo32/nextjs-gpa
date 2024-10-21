4faac8f253a5cbbcc5d61a8c95febffbbdea56df
gardodaqqaq <gardodaqqaq@gmail.com>
2024-10-21 06:20:27 +0000

    Added exams Tab

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs"
 import GPACalculator from '@/components/GPACalculator'
 import Timetable from '@/components/Timetable'
@@ -6,9 +7,14 @@ export default function Home() {
   return (
     <div className="w-full max-w-4xl mx-auto">
       <Tabs defaultValue="timetable" className="w-full">
-        <TabsList className="grid w-full grid-cols-2">
+        <TabsList className="grid w-full grid-cols-3">
           <TabsTrigger value="timetable">Timetable</TabsTrigger>
           <TabsTrigger value="gpa">GPA Calculator</TabsTrigger>
+          <Link href="https://nasservocational-my.sharepoint.com/:x:/g/personal/mussab_aswad_nvtc_edu_bh/EfVGxHZP6J5Jia0sHIsd0SwBTKKbDtEq4r4SFJ4tullmmA?e=dx9kpT&nav=MTVfe0M2RjJFMEQ4LUI2NTMtMEI0MC1CMDExLTVFRUJDNzYwRjMxQX0" passHref legacyBehavior>
+            <TabsTrigger value="resources" asChild>
+              <a>Resources</a>
+            </TabsTrigger>
+          </Link>
         </TabsList>
         <TabsContent value="timetable" className="mt-6">
           <Timetable />
